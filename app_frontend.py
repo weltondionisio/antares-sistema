55613c1cec5c5fbf76efe001c756d1bcfbd08d4f
Welton Dionisio <113948083+weltondionisio@users.noreply.github.com>
2026-08-10 15:06:28 -0300

    Refactor app_frontend.py to improve structure

--- app_frontend.py
+++ app_frontend.py
@@ -181,7 +181,8 @@ with col2:
 
 st.markdown("---")
 st.markdown(
-    "🛠️ **Sistema ANTARES (Automated Neuroevolutionary Tool for Anticipating Risk of Envenomation by Scorpions)** — Uma Ferramenta Neuroevolutiva Automatizada para Antecipar o Risco de Envenonamento por Escorpiões.<br>"
+    "🛠️ **Sistema ANTARES (Automated Neuroevolutionary Tool for Anticipating Risk of Envenomation by Scorpions)**<br>"
+    "Uma Ferramenta Neuroevolutiva Automatizada para Antecipar o Risco de Envenonamento por Escorpiões.<br>"
     "Autores: Dr. Welton Dionisio-da-Silva (USP) e Dr. Rodrigo Hirata Willemart (USP).<br>"
     "Financiamento: São Paulo Research Foundation (FAPESP), Brazil, process number #2024/07110-0.",
     unsafe_allow_html=True
